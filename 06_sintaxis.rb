fa4c4ff45aea56c7f27b292d70ffdf5e80c0510f
Sergio Lopez <lopez.sergio.n@gmail.com>
2021-12-15 11:58:13 -0300

    sintaxis

---
+++ 06_sintaxis.rb
@@ -0,0 +1,9 @@
+# En Ruby no se utiliza punto y coma para separar sentencias
+# Los cometarios se escriben con # al principio de la línea
+=begin
+  Esto es un comentario
+  multilínea
+=end
+# Los paréntesis son opcionales
+# Las variables y métodos se escriben en snake_case
+# Los nombres de las clases se escriben en camelCase con la primera letra en mayúscula
