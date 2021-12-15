11a55ed4a96aaab6100a50f0dd2fc63dafe60693
Sergio Lopez <lopez.sergio.n@gmail.com>
2021-12-15 12:12:02 -0300

    comparison operators

---
+++ 07_operadores_de_comparacion.rb
@@ -0,0 +1,20 @@
+# Las comparaciones retornan un valor booleano (true o false)
+# < menor que
+# > mayor que
+# <= menor o igual que
+# >= mayor o igual que
+# == igual que
+# != diferente que
+# === igual y del mismo tipo
+# !== diferente y del mismo tipo
+# 1.eql?(1.0) => false
+# 1.eql?(1) => true
+# "hola".equal?("hola") => false (no son el mismo objeto, tienen diferente id)
+
+
+# <=> operador de comparación combinado
+
+20 <=> 10 # => 1
+10 <=> 20 # => -1
+10 <=> 10 # => 0
+
