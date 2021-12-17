6761b90846177b9918282796ef19ad60aca65e4a
Sergio Lopez <lopez.sergio.n@gmail.com>
2021-12-17 00:36:18 -0300

    arithmetic operators

---
+++ 08_operadores_aritmeticos.rb
@@ -0,0 +1,25 @@
+# + suma
+# - resta
+# * multiplicación
+# / división
+# % modulo
+# ** potencia
+# += suma y asigna
+# -= resta y asigna
+# *= multiplicación y asigna
+# /= división y asigna
+# %= modulo y asigna
+# **= potencia y asigna
+
+# Presedencias de los operadores
+# ** potencia
+
+# * multiplicación
+# / división
+# % modulo
+
+# + suma
+# - resta
+
+# Si se usan operadores de misma presedencia, se ejecutan de izquierda a derecha
+
