776b33d9268099b9b3dffa9f81f14cecd14bc5cb
Sergio Lopez <lopez.sergio.n@gmail.com>
2021-12-17 01:15:40 -0300

    ternary

---
+++ 11_operador_ternario.rb
@@ -0,0 +1,22 @@
+user = "Sergio"
+
+# if user == "Sergio"
+#   puts "Hola Sergio"
+# else
+#   puts "Hola desconocido"
+# end
+
+# puts (if user == "Sergio" then "Hola Sergio" else "Desconocido" end)
+
+# respuesta = if user == "Sergio"
+#   then "Sergio"
+# else
+#   "Desconocido"
+# end
+
+# puts respuesta
+
+
+# Operator ternario
+
+puts user == "Sergio" ? "Hola Sergio" : "Hola desconocido"
