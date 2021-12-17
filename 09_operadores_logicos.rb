d0bef9e6e03cb9e249d3307db2a6b1b3cb9fdc36
Sergio Lopez <lopez.sergio.n@gmail.com>
2021-12-17 00:36:40 -0300

    logic operators

---
+++ 09_operadores_logicos.rb
@@ -0,0 +1,14 @@
+# Los operadores lógicos devuelven true or false
+# Ordenados por orden de precedencia
+
+# ! negación
+# && y
+# || o
+
+# Los símbolos tienen mayor precedencia que las palabras
+
+# not negación
+# and y
+# or o
+
+
