b309da547a3da48ff115799e8f180f6c3ebc4027
Sergio Lopez <lopez.sergio.n@gmail.com>
2022-01-02 01:09:35 -0300

    array operations

---
+++ 15_operaciones_con_arreglos.rb
@@ -0,0 +1,23 @@
+calificaciones = %w[10 9 8 7 6 5 4 3 2 1]
+
+# Multiplicación de arreglos
+puts calificaciones * 2  #Salida: "10 9 8 7 6 5 4 3 2 1 10 9 8 7 6 5 4 3 2 1"
+
+# Multiplicar por string (lo que se hace es un join)
+puts calificaciones * " - " #Salida: "10 - 9 - 8 - 7 - 6 - 5 - 4 - 3 - 2 - 1"
+puts calificaciones.join(" - ") #Salida: "10 - 9 - 8 - 7 - 6 - 5 - 4 - 3 - 2 - 1"
+
+
+# Join convierte un arreglo en una cadena
+# Join recibe como argumento un string que separa los elementos del arreglo.
+puts calificaciones.join("") #Salida: "10987654321"
+puts calificaciones.join(" - ") #Salida: "10 - 9 - 8 - 7 - 6 - 5 - 4 - 3 - 2 - 1"
+
+
+# Para que identifique que el arreglo contiene números.
+calificaciones = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+puts calificaciones.sort
+puts calificaciones.sort.reverse
+
+# Buscar un elemento en un arreglo
+puts calificaciones.include?(10) #Salida: true
