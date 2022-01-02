e59c0e737ca193de5c75e6ed7994879493ac473b
Sergio Lopez <lopez.sergio.n@gmail.com>
2022-01-02 16:47:19 -0300

    times upto downto

---
+++ 18_mas_iteradores.rb
@@ -0,0 +1,18 @@
+# Times, upto, downto
+
+# Un interador es un método que internamente construye un ciclo.
+
+# 1-10
+
+10.times do |i|
+  puts i
+end
+
+1.upto(10) do |i|
+  puts i
+end
+
+10.downto(1) do |i|
+  puts i
+end
+
